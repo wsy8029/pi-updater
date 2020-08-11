f7887db5f09fc990241d71908910358c7a7e60e6
wsy8029 <wsy8029@gmail.com>
2020-08-11 16:22:07 +0900

    Implement Update Config and Code

--- updater.sh
+++ updater.sh
@@ -1,10 +1,30 @@
 #!bin/bash
-tmp=`iwconfig wlan0|grep ESSID:off`
-num="${#tmp}" 
-if [ $num = "0" ]
-then
-wlan=true
-else
-wlan=false
-fi
+
+#check wifi connection and return boolean
+check_wifi()
+{
+	tmp=`iwconfig wlan0|grep ESSID:off`
+	num="${#tmp}" 
+	if [ $num = "0" ]
+	then
+	wlan=true
+	else
+	wlan=false
+	fi
+	echo $wlan
+}
+wlan=$(check_wifi)
 echo $wlan
+
+#update config and code when wlan is true
+if [ $wlan = true ]
+then
+echo TRUE
+sudo /bin/bash ./update_config.sh
+sudo /bin/bash ./update_code.sh 
+else
+echo FALSE
+echo NETWORK NOT AVAILABLE
+fi
+
+
