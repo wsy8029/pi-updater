fb4ebc3f3729b55d519d78f20599bf5bd3f96f18
wsy8029 <wsy8029@gmail.com>
2020-08-13 12:38:29 +0900

    Test version check

--- updater.sh
+++ updater.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
+path=/home/pi/
 # generate update_log.txt file at workspace folder
 sudo touch /home/pi/workspace/update_log.txt
 sudo chmod 666 /home/pi/workspace/update_log.txt
@@ -9,11 +10,10 @@ check_wifi()
 {
 	tmp=`iwconfig wlan0|grep ESSID:off`
 	num="${#tmp}" 
-	if [ $num = "0" ]
-	then
-	wlan=true
+	if [ $num = "0" ]; then
+		wlan=true
 	else
-	wlan=false
+		wlan=false
 	fi
 	echo $wlan
 }
@@ -21,7 +21,13 @@ check_wifi()
 # check local updater version compare with latest github version 
 check_version()
 {
-	
+	ver_latest=`curl https://raw.githubusercontent.com/wsy8029/pi-updater/master/version`
+	ver_local=$(<version)	
+	if [ $ver_latest == $ver_local ]; then
+		echo dd > ${path}same
+	else
+		echo dd > diff
+	fi
 }
 
 
@@ -31,13 +37,13 @@ logger()
 	msg=$1
 	sudo echo -e `date`: $msg >> /home/pi/workspace/update_log.txt 
 }
-
+$(check_version)
 # update config and code when wlan is true
 while [ true ]; do
 
 	wlan=$(check_wifi)
 	if [ $wlan = true ]; then
-		sudo python3 /home/pi/pi-upddater/led/on_blue.py
+		sudo python3 /home/pi/pi-updater/led/on_blue.py
 		$(logger "wifi enable")
 		sudo /bin/bash /home/pi/pi-updater/update_config.sh
 		$(logger "config updated")
