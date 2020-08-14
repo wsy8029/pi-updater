28f1856ef88d66ad6406808d3d91f76c89ba5de4
wsy8029 <wsy8029@gmail.com>
2020-08-14 14:33:01 +0900

    Fix to get latest version and save after update

--- updater.sh
+++ updater.sh
@@ -7,6 +7,9 @@ path_workspace=/home/pi/workspace/
 sudo touch ${path_workspace}.update_log
 sudo chmod 666 ${path_workspace}.update_log
 
+# chmod version file to update
+sudo chmod 666 ${path_updater}version
+
 # save log with time
 logger()
 {
@@ -31,11 +34,17 @@ check_wifi()
 check_version()
 {
 	#ver_latest=`sudo curl https://raw.githubusercontent.com/wsy8029/pi-updater/master/version`
-	ver_latest=$(wget https://raw.githubusercontent.com/wsy8029/pi-updater/master/version -q -O -)
+	while [ "$ver_latest" == "" ]; do
+		$(logger "[VERSION] cannot get latest version, retry...")
+		sudo python3 ${path_updater}led/on_red.py
+		ver_latest=$(wget https://raw.githubusercontent.com/wsy8029/pi-updater/master/version -q -O -)
+		sudo python3 ${path_updater}led/off.py
+	done
+	#ver_latest=$(wget https://raw.githubusercontent.com/wsy8029/pi-updater/master/version -q -O -)
 	$(logger "[VERSION] latest version is $ver_latest")
 	ver_local=$(<${path_updater}version)	
 	$(logger "[VERSION] current local version is $ver_local")
-	if [ $ver_latest == $ver_local ]; then
+	if [ "$ver_latest" == "$ver_local" ]; then
 		latest=true
 	else
 		latest=false
@@ -47,11 +56,11 @@ check_version()
 while [ true ]; do
 
 	wlan=$(check_wifi)
-	if [ $wlan = true ]; then
+	if [ $wlan == true ]; then
 		sudo python3 ${path_updater}led/on_blue.py
 		$(logger "[WIFI] wifi enable")
 		latest=$(check_version)
-		if [ $latest = true ]; then
+		if [ $latest == true ]; then
 			$(logger "[VERSION] local version is already up to date.")
 			sudo python3 ${path_updater}led/blink_rgb1.py
 			break
@@ -61,7 +70,9 @@ while [ true ]; do
 			$(logger "[UPDATE] config updated")
 			sudo /bin/bash ${path_updater}update_code.sh 
 			$(logger "[UPDATE] code updated")
-			$(logger "[UPDATE] Update complete" )
+			ver_updated=$(wget https://raw.githubusercontent.com/wsy8029/pi-updater/master/version -q -O -)
+			$(logger "[UPDATE] Update complete (ver : $ver_updated )" )
+			sudo echo $ver_updated > ${path_updater}version
 			sudo python3 ${path_updater}led/blink_rgb1.py
 			break
 		fi
